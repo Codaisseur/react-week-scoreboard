1afd91f9ccb0055413e8ed339a84024b661d8503
Domenico Gemoli <domenicogemoli@gmail.com>
2021-02-25 16:03:47 +0100

    Sort players

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,7 +2,9 @@
 export default function Player(props) {
   return (
     <li className="Player">
-      <p>{props.name}</p>
+      <p>
+        {props.name} (score: {props.score})
+      </p>
     </li>
   );
 }
